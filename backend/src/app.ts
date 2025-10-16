65df328737597a9d5f801ad45883aaf88da65f9c
kuslhhh <kushaljadhav7745@gmail.com>
2025-10-17 02:34:50 +0530

    env setup

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import authRouter from "./modules/auth/route"
+
+export const app = express();
+
+app.use(cors)
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/auth", authRouter)
---
+++ backend/src/config/env.ts
@@ -0,0 +1,14 @@
+import "dotenv/config"
+
+export const env = {
+   PORT: process.env.PORT || 5000,
+   DATABASE_URL: process.env.DATABASE_URL!,
+   ACCESS_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET!,
+   REFRESH_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET!,
+   ACCESS_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN || "15m",
+   REFRESH_EXPIRES_DAYS: parseInt(process.env.REFRESH_TOKEN_EXPIRES_IN_DAYS || "30"),
+   BCRYPT_SALT_ROUNDS: parseInt(process.env.BCRYPT_SALT_ROUNDS || "12")
+}
+
+
+console.log(env)
