55dd63649b22e5e6e064970dacce398a9b9e06f1
kuslhhh <kushaljadhav7745@gmail.com>
2025-10-17 01:52:13 +0530

    re-init

--- backend/src/utils/authSchema.ts
+++
@@ -1,8 +0,0 @@
-import {z} from "zod"
-
-const PASSREGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/
-
-export const authSchema = z.object({
-   email: z.email({message: "Invalid email address"}),
-   password: z.string().regex(PASSREGEX)
-})
--- backend/src/utils/generateToken.ts
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export const generateToken = (userId: number) => {
-   const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET!, { expiresIn: "15m" })
-   const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET!, { expiresIn: "7d" })
-
-   return { accessToken, refreshToken }
-}
